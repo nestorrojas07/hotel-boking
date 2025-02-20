623175a7b34318446bfaea142c147d8220e6651f
Nestor Rojas <nestorrojas07@gmail.com>
2025-02-20 12:09:45 -0500

    add extension to get current user from HttpContext

---
+++ HotelBooking/Extensions/AuthExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Domain.Dtos;
+
+namespace HotelBooking.Extensions;
+
+public static class AuthExtension
+{
+    public static UserPrincipal CurrentUser(this ClaimsPrincipal principal) {
+
+        foreach (var claim in principal.Claims)
+        {
+            Console.WriteLine($"{claim.Type}  =>   '{claim.Value}'");
+        }
+        var user = new UserPrincipal();
+        user.Id = int.Parse(principal.Claims.First(i => i.Type == "userId").Value);
+        user.Email = principal.Claims.First(i => i.Type == ClaimTypes.Email).Value;
+        user.Role = principal.Claims.First(i => i.Type == ClaimTypes.Role).Value;
+        user.FullName = principal.Claims.First(i => i.Type == ClaimTypes.Name).Value;
+
+        return user;
+    }
+}
